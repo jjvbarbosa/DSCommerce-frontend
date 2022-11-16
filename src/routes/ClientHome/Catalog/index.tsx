f4c07b12b38f2080fe4195f1957923d4c06e5387
João Vitor Barbosa <joaovitor@jv>
2022-11-16 15:39:53 -0300

    First back end request

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -4,15 +4,27 @@ import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from '../../../services/product-service';
+import { ProductDTO } from "../../../models/product";
+import { useEffect, useState } from "react";
 
 export default function Catalog() {
+
+    const [products, setProducts] = useState<ProductDTO[]>([]);
+
+    useEffect(() => {
+        productService.findAll()
+            .then(response => {
+                setProducts(response.data.content);
+            });
+    }, []);
+
     return (
         <main>
             <section id="catalog-section" className="dsc-container">
                 <SearchBar />
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                     {
-                        productService.findAll().map(
+                        products.map(
                             product => <CatalogCard key={product.id} product={product} />
                         )
                     }
